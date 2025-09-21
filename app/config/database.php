231b3cae64fb90465423b1de6a58da367d0d180a
ckanzoo <pickanzo@gmail.com>
2025-09-21 13:36:06 +0800

    up

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799452',
-    'password'	=> 'hyPNQRFAtn',
-    'database'	=> 'sql12799452',
+    'username'	=> 'sql12799459',
+    'password'	=> 'rxyLX6y76k',
+    'database'	=> 'sql12799459',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
